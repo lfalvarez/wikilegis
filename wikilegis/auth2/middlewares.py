64d88dc91435a67105f5a9474cb8e69559df324a
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-06-06 10:06:13 -0300

    Added email to remote user
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- wikilegis/auth2/middlewares.py
+++ wikilegis/auth2/middlewares.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.middleware import RemoteUserMiddleware
-from Cookie import SimpleCookie, Morsel
-import copy
-
+import json
 
 
 class WikilegisRemoteUser(RemoteUserMiddleware):
     header = "HTTP_AUTH_USER"
+
+    def process_request(self, request):
+        super(WikilegisRemoteUser, self).process_request(request)
+        if request.user.is_authenticated():
+            user = request.user
+            user_data = json.loads(request.META.get('HTTP_REMOTE_USER_DATA'))
+            user.email = user_data['email']
+            user.first_name = user_data['name']
